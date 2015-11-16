bc91a2918f03339cdd6bfc537abbfe26578c190d
Kushan Joshi <0o3ko0@gmail.com>
2015-11-16 22:19:48 +0530

    ojo

--- audio.c
+++ audio.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <signal.h>
 #include <pthread.h>
 #include "library.h"
 
-static void *listen() {
-
+void *listener() {
+    return NULL;
 }
 void audio(char *peer, char *port) {
     printf("%s %s\n", peer, port);
     pthread_t thr_listen;
-    pthread_create(&thr_listen, NULL, &listen, port);
+    pthread_create(&thr_listen, NULL, &listener, port);
 }
--- library.h
+++ library.h
@@ -1,14 +1,6 @@
-#include <stdio.h>
-#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
-typedef struct {
-  int socket;
-  struct sockaddr_in addr;
-  socklen_t addr_len;
-} connection_t;
+#include <pthread.h>
 
 void test();
 int arg_finder(char *match,int argc, char *argv[]);
-int connection_init(int port, int *sockfd);
